bbdd519e40611d80f22f741df92c171800413cde
MrGreensWorkshop <76992231+MrGreensWorkshop@users.noreply.github.com>
2022-04-15 16:03:03 +0900

    version up

--- assets/js/_js/post-list/post-query.js
+++ assets/js/_js/post-list/post-query.js
@@ -1,4 +1,4 @@
-/*! Mr. Green Jekyll Theme - v1.0.0 (https://github.com/MrGreensWorkshop/MrGreen-JekyllTheme)
+/*! Mr. Green Jekyll Theme - v1.0.1 (https://github.com/MrGreensWorkshop/MrGreen-JekyllTheme)
  *  Copyright (c) 2022 Mr. Green's Workshop https://www.MrGreensWorkshop.com
  *  Licensed under MIT
 */
